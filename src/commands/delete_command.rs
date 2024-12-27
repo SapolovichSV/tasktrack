fb234f30678909b786c75971fd2920711eceebf4
Stasnyash <ballplayerr44@gmail.com>
2024-12-27 16:27:47 +1000

    make delete_command_working

--- src/commands/delete_command.rs
+++ src/commands/delete_command.rs
@@ -1,4 +1,18 @@
+use crate::config::config;
+use crate::storage::ModifyStorage;
+
+use super::Command;
 pub struct DeleteCommand {
-    config: Config,
+    config: config::Config,
     storage: Box<dyn ModifyStorage>,
+}
+impl Command for DeleteCommand {
+    fn execute(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let delete_id = match self.config.task_id {
+            Some(id) => id,
+            None => return Err("Task id is required".into()),
+        };
+        self.storage.delete_task(&delete_id)?;
+        Ok(())
+    }
 }
